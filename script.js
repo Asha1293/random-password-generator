ad284284572400cc0c89c945df613fde44c2145d
Asha jacob <asha.j.1993@gmail.com>
2024-02-08 21:49:00 +0100

    updated files

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let passwordBox = document.getElementById("password")
 
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const symbols = "~`! @#$%^&*()_-+={[}]|\:;'<,>.?/"
+const length = 12
+
+
+
+const allChars = upperCase + lowerCase + numbers + symbols
+
+function generatePassword(){
+    let passWord = ""
+    passWord += upperCase[Math.floor(Math.random()*upperCase.length)]
+    passWord += lowerCase[Math.floor(Math.random()*lowerCase.length)]
+    passWord += numbers[Math.floor(Math.random()*numbers.length)]
+    passWord += symbols[Math.floor(Math.random()*symbols.length)]
+
+    while(length> passWord.length){
+        passWord += allChars[Math.floor(Math.random()*allChars.length)]
+    }
+    
+    passwordBox.value = passWord
+
+}
+
+function copyPassword(){
+     passwordBox.select()
+     document.execCommand("copy")
+}
